60544dee87587755722df567a7246b3f5ee6cab7
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-02 16:07:56 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,8 +7,8 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-    if @author.valid? 
+    @author = Author.new(author_params)
+    if @author.save 
     redirect_to author_path(@author)
     else 
       render :new 
